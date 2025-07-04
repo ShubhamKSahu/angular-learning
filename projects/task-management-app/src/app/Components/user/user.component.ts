923e94540c28f11d1734056777ad14271a8cf0cf
ShubhamKSahu <shubham.official571@gmail.com>
2025-07-04 23:31:59 +0530

    type alias and interfaces

--- projects/task-management-app/src/app/Components/user/user.component.ts
+++ projects/task-management-app/src/app/Components/user/user.component.ts
@@ -1,17 +1,25 @@
 import { Component, Input, Output, EventEmitter} from '@angular/core';
 import { users } from 'src/app/dummy-user';
 
+
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name : string
+// }
+
+interface User {
+  id: string;
+  avatar: string;
+  name : string
+}
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent {
-  @Input({required: true}) user !: {
-    id: string;
-    avatar : string;
-    name: string
-  }
+  @Input({required: true}) user !: User;
   @Output() select = new EventEmitter();
   //select = output<string>();
   get imagePath(){
