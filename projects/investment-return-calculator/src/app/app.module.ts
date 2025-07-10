692406a87a38659ce9d552bc86631d38c12e96d3
ShubhamKSahu <shubham.official571@gmail.com>
2025-07-10 12:07:17 +0530

    Header component added

--- projects/investment-return-calculator/src/app/app.module.ts
+++ projects/investment-return-calculator/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { UserInputComponent } from './components/user-input/user-input.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
