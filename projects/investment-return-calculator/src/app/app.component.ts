719725572a2244361f45f68798a6b19d80f52673
ShubhamKSahu <shubham.official571@gmail.com>
2025-07-12 00:21:56 +0530

    Added calculate logic on submit

--- projects/investment-return-calculator/src/app/app.component.ts
+++ projects/investment-return-calculator/src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
-
+import { InvestmentInput } from './components/user-input/user-input.model';
 @Component({
   selector: 'app-root',
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'investment-return-calculator';
+
+calculateInvestmentResults(data: InvestmentInput) {
+  const {initialInvestment, annualInvestment, expectedReturn, duration} = data;
+  const annualData = [];
+  let investmentValue = initialInvestment;
+
+  for (let i = 0; i < duration; i++) {
+    const year = i + 1;
+    const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+    investmentValue += interestEarnedInYear + annualInvestment;
+    const totalInterest =
+      investmentValue - annualInvestment * year - initialInvestment;
+    annualData.push({
+      year: year,
+      interest: interestEarnedInYear,
+      valueEndOfYear: investmentValue,
+      annualInvestment: annualInvestment,
+      totalInterest: totalInterest,
+      totalAmountInvested: initialInvestment + annualInvestment * year,
+    });
+  }
+
+  //return annualData;
+  console.log(annualData); 
+}
 }
--- projects/investment-return-calculator/src/app/components/user-input/user-input.component.ts
+++ projects/investment-return-calculator/src/app/components/user-input/user-input.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, Output, EventEmitter } from '@angular/core';
+import { InvestmentInput } from './user-input.model';
 @Component({
   selector: 'app-user-input',
+  standalone: false,
   templateUrl: './user-input.component.html',
   styleUrls: ['./user-input.component.css']
 })
 export class UserInputComponent {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedInvestment = '0';
+  enteredDuration = '0'
+  @Output() calculate = new EventEmitter<InvestmentInput>();
   onSubmit(){
-    alert("Submitted!")
+    this.calculate.emit({
+      initialInvestment : +this.enteredInitialInvestment,
+      annualInvestment : +this.enteredAnnualInvestment,
+      expectedReturn : +this.enteredInitialInvestment,
+      duration: +this.enteredDuration
+    })
   }
 }
