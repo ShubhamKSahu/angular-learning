ba2882c02e61cc5036ec04d086ce8aa958da9b07
ShubhamKSahu <shubham.official571@gmail.com>
2025-07-21 18:02:03 +0530

    components added

--- projects/inventory-management-app/src/app/app.component.ts
+++ projects/inventory-management-app/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './component/header/header.component';
-import { ServerStatusComponent } from './component/server-status/server-status.component';
-import { TrafficComponent } from './component/traffic/traffic.component';
-import { SupportTicketsComponent } from './component/support-tickets/support-tickets.component';
+import { ServerStatusComponent } from './component/dashboard/server-status/server-status.component';
+import { TrafficComponent } from './component/dashboard/traffic/traffic.component';
+import { SupportTicketsComponent } from './component/dashboard/support-tickets/support-tickets.component';
+import { DashboardItemComponent } from "./component/dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, SupportTicketsComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, SupportTicketsComponent, DashboardItemComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
